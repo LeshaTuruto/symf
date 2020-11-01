c267ac5d9c174a618b597aca9c85a44270dc3f8d
Kenfordovec <NikMen570@mail.ru>
2020-11-01 09:57:55 +0300

    security

---
+++ src/Form/UserType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use function Sodium\add;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, array(
+                'label' => 'Enter email'
+            ))
+            ->add('plainPassword', RepeatedType::class, array(
+                    'type' => PasswordType::class,
+                    'first_options' => array(
+                        'label' => 'password',
+                    ),
+                    'second_options' => array(
+                        'label' => 'Repeat the password',
+                    ),
+                ))
+            ->add('confirm', SubmitType::class, array('label' => 'Confirm'));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
